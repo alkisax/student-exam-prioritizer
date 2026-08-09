73bfa192c922d0dd82b1f0171a3e13215b852f43
alkisax <alkisax@gmail.com>
2026-08-09 13:47:58 +0300

    wip: dark theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./authLogin/context/UserAuthContext";
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './theme/ThemeContext'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>,
